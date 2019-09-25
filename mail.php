81c32b3c816f7c52d560e06308a2821a942b35b9
Suraj Neupane <jarusp29@users.noreply.github.com>
2019-09-25 21:49:42 +0300

    create
    great

---
+++ mail.php
@@ -0,0 +1,13 @@
+<?PHP
+    $to = "jarusp29@gmail.com";
+    $subject ="Respose from Website";
+    $message = "Im very thankful to you";
+    $headers ="From: jarusp29@users.noreply.github.com";
+
+if(mail($to, $subject, $message, $headers)){
+    echo "mail Sent successfully";
+}
+    else{
+    echo "can not sent mail";
+}
+?>
