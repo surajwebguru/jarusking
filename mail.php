bbce9c5b5bdfae21cf13f132f697aa338f6a453a
Suraj Neupane <31807275+surajwebguru@users.noreply.github.com>
2019-09-25 22:07:44 +0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,8 +1,8 @@
-<?PHP
+<?php
     $to = "jarusp29@gmail.com";
     $subject ="Respose from Website";
     $message = "Im very thankful to you";
-    $headers ="From: jarusp29@users.noreply.github.com";
+    $headers = "From: jarusp29@users.noreply.github.com";
 
 if(mail($to, $subject, $message, $headers)){
     echo "mail Sent successfully";
